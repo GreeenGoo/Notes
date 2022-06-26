758ed39e787e4d7acb96d8f95bfab1dc17f2fbdd
PC <uladzislau.krukosuki@gmail.com>
2022-06-26 21:02:06 +0300

    Created bottomNavigationView for switch fragments and fragmentContainerView for show fragments. There are two fields in bottomNavigationView: "Notes" and "Tasks". Menu works.

--- app/src/main/java/com/education/notes/MainActivity.kt
+++ app/src/main/java/com/education/notes/MainActivity.kt
@@ -2,10 +2,45 @@ package com.education.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.education.notes.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container_view, NotesFragment())
+            .commit()
+
+        val bottomNavView: BottomNavigationView = binding.bottomNavigationView
+        initBottomNavView(bottomNavView)
+    }
+
+    private fun initBottomNavView(bottomNav: BottomNavigationView){
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.bottom_navigation_menu_button_notes -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragment_container_view, NotesFragment())
+                        .commit()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.bottom_navigation_menu_button_tasks -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragment_container_view, TasksFragment())
+                        .commit()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
+        bottomNav.setOnItemReselectedListener{}
     }
 }
