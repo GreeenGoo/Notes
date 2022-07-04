df17a26dcbaa88c2ba6eeace15743c30426edfd5
PC <uladzislau.krukosuki@gmail.com>
2022-07-04 16:30:53 +0300

    Created NavigationController component, created connection between fragments.

--- app/src/main/java/com/education/notes/MainActivity.kt
+++ app/src/main/java/com/education/notes/MainActivity.kt
@@ -1,46 +1,23 @@
 package com.education.notes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.education.notes.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment_container_view, NotesFragment())
-            .commit()
-
-        val bottomNavView: BottomNavigationView = binding.bottomNavigationView
-        initBottomNavView(bottomNavView)
-    }
-
-    private fun initBottomNavView(bottomNav: BottomNavigationView){
-        bottomNav.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.bottom_navigation_menu_button_notes -> {
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.fragment_container_view, NotesFragment())
-                        .commit()
-                    return@setOnItemSelectedListener true
-                }
-                R.id.bottom_navigation_menu_button_tasks -> {
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.fragment_container_view, TasksFragment())
-                        .commit()
-                    return@setOnItemSelectedListener true
-                }
-            }
-            false
-        }
-        bottomNav.setOnItemReselectedListener{}
+        val navController =
+            (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment)
+                .navController
+        val bottomNavView = binding.bottomNavigationView
+        bottomNavView.setupWithNavController(navController)
     }
 }
