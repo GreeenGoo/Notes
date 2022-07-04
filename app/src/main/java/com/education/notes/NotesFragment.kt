9997da90e2c6ec2fb999791889ea8e7150f70ed2
PC <uladzislau.krukosuki@gmail.com>
2022-07-04 16:49:26 +0300

    add empty method addNote()

--- app/src/main/java/com/education/notes/NotesFragment.kt
+++ app/src/main/java/com/education/notes/NotesFragment.kt
@@ -16,7 +16,15 @@ class NotesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_notes, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        addNote()
+    }
+
+    private fun addNote(){
+
     }
 }
