8bc8e06c517dd64d1cebacc0cb8e4ab38f72d8b3
PC <uladzislau.krukosuki@gmail.com>
2022-07-14 19:04:08 +0300

    Called a method for ActionBar label changing.

--- app/src/main/java/com/education/notes/presentation/MainActivity.kt
+++ app/src/main/java/com/education/notes/presentation/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.education.notes.presentation
 
+//import androidx.navigation.findNavController
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-//import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.education.notes.R
 import com.education.notes.databinding.ActivityMainBinding
 
@@ -14,12 +15,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //supportActionBar?.hide()
-       /* val navController =
-            (supportFragmentManager.findFragmentById(R.id.nav_graph) as NavHostFragment)
-                .navController*/
-        //findNavController().navigate(R.id.list_fragment)
-        /*val bottomNavView = binding.bottomNavigationView
-        bottomNavView.setupWithNavController(navController)*/
+
+        supportFragmentManager.findFragmentById(R.id.fragmentContainerView)?.findNavController()
+            ?.let {
+                setupActionBarWithNavController(
+                    it
+                )
+            }
     }
 }
