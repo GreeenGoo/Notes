caf75788774b5eb93531f5396f65ff142bab2d4f
PC <uladzislau.krukosuki@gmail.com>
2022-07-15 19:30:50 +0300

    Created RecyclerView and ViewModel for ListFragment.kt. !!Working version of showing data!!

--- app/src/main/java/com/education/notes/presentation/fragments/notes/list/ListFragment.kt
+++ app/src/main/java/com/education/notes/presentation/fragments/notes/list/ListFragment.kt
@@ -5,14 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.education.notes.R
 import com.education.notes.databinding.FragmentListBinding
+import com.education.notes.presentation.data.UserViewModel
 
 class ListFragment : Fragment() {
 
     private var _binding: FragmentListBinding? = null
     private val binding get() = _binding!!
+    private lateinit var mUserViewModel: UserViewModel
 
 
     override fun onCreateView(
@@ -21,6 +26,18 @@ class ListFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         _binding = FragmentListBinding.inflate(inflater, container, false)
+
+        //RecyclerView
+        val adapter = ListAdapter()
+        val recyclerView = _binding!!.recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        //UserViewModel
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        mUserViewModel.readAllData.observe(viewLifecycleOwner, Observer{user ->
+            adapter.setData(user)
+        })
 
         binding.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.nav_graph_add_fragment)
