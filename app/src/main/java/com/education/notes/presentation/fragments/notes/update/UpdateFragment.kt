7d307a6f19dd6197796506654dcfa4d4be517b26
PC <uladzislau.krukosuki@gmail.com>
2022-07-15 20:22:48 +0300

    Modified UpdateFragment.kt. When list's column with some user was clicked, UpdateFragment.kt has an information about the user to update it. !!Update button doesn't work!!

--- app/src/main/java/com/education/notes/presentation/fragments/notes/update/UpdateFragment.kt
+++ app/src/main/java/com/education/notes/presentation/fragments/notes/update/UpdateFragment.kt
@@ -1,19 +1,30 @@
 package com.education.notes.presentation.fragments.notes.update
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.education.notes.R
+import com.education.notes.databinding.FragmentUpdateBinding
 
 class UpdateFragment : Fragment() {
+    private var _binding: FragmentUpdateBinding? = null
+    private val binding get() = _binding!!
+    private val args by navArgs<UpdateFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false)
+        _binding =  FragmentUpdateBinding.inflate(inflater, container, false)
+
+        binding.upgradeFragmentFirstName.setText(args.currentUser.firstName)
+        binding.upgradeFragmentLastName.setText(args.currentUser.lastName)
+        binding.upgradeFragmentAge.setText(args.currentUser.age.toString())
+
+        return binding.root
     }
 }
