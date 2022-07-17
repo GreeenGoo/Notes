30468b5f47c66d0a1f681c1291b5858c5d077f65
PC <uladzislau.krukosuki@gmail.com>
2022-07-17 17:40:43 +0300

    Realized deleting all the user.

--- app/src/main/java/com/education/notes/presentation/fragments/notes/list/ListFragment.kt
+++ app/src/main/java/com/education/notes/presentation/fragments/notes/list/ListFragment.kt
@@ -1,10 +1,13 @@
 package com.education.notes.presentation.fragments.notes.list
 
+import android.app.AlertDialog
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -43,5 +46,43 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.nav_graph_add_fragment)
         }
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.delete_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.menu_delete -> {
+                        deleteAllUsers()
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun deleteAllUsers() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes") { _, _ ->
+            mUserViewModel.deleteAllUsers()
+            Toast.makeText(
+                requireContext(),
+                "Successfully removed everything!",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        builder.setNegativeButton("No") { _, _ ->
+
+        }
+        builder.setTitle("Delete everything?")
+        builder.setMessage("Are you sure you want to delete all the users")
+        builder.create().show()
     }
 }
