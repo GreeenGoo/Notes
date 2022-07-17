f042165812015a956d25ff6f2cc44792ba616030
PC <uladzislau.krukosuki@gmail.com>
2022-07-17 16:53:16 +0300

    Created an "Delete" menu and an AliertDialog menu for deleting some line. There us no implementation.

--- app/src/main/java/com/education/notes/presentation/fragments/notes/update/UpdateFragment.kt
+++ app/src/main/java/com/education/notes/presentation/fragments/notes/update/UpdateFragment.kt
@@ -1,13 +1,15 @@
 package com.education.notes.presentation.fragments.notes.update
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextUtils
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -21,14 +23,12 @@ class UpdateFragment : Fragment() {
     private val binding get() = _binding!!
     private val args by navArgs<UpdateFragmentArgs>()
     private lateinit var mUserViewModel: UserViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         _binding = FragmentUpdateBinding.inflate(inflater, container, false)
-
         mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
         binding.upgradeFragmentFirstName.setText(args.currentUser.firstName)
         binding.upgradeFragmentLastName.setText(args.currentUser.lastName)
@@ -41,6 +41,39 @@ class UpdateFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.delete_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.menu_delete -> {
+                        deleteUser()
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+    }
+
+    private fun deleteUser() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes") { _, _ ->
+
+        }
+        builder.setNegativeButton("No") { _, _ ->
+
+        }
+        builder.setTitle("Delete ${args.currentUser.firstName}?")
+        builder.setMessage("Are you sure you want to delete ${args.currentUser.firstName}")
+        builder.create().show()
+    }
+
     private fun updateItem() {
         val firstName = binding.upgradeFragmentFirstName.text.toString()
         val lastName = binding.upgradeFragmentLastName.text.toString()
@@ -51,11 +84,19 @@ class UpdateFragment : Fragment() {
             val updateUser = User(args.currentUser.id, firstName, lastName, age)
             //Update Current User
             mUserViewModel.updateUser(updateUser)
-            Toast.makeText(requireContext(), "Successfully updated!", Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                requireContext(),
+                "Successfully updated!",
+                Toast.LENGTH_LONG
+            ).show()
             //NavigateBack
             findNavController().navigate(R.id.nav_graph_list_fragment)
         } else {
-            Toast.makeText(requireContext(), "Please, fill out all the fields.", Toast.LENGTH_LONG)
+            Toast.makeText(
+                requireContext(),
+                "Please, fill out all the fields.",
+                Toast.LENGTH_LONG
+            )
                 .show()
         }
     }
