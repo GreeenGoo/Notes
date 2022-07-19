6aa7d834d286cf45af94e23631ac73343dba8960
PC <uladzislau.krukosuki@gmail.com>
2022-07-19 17:34:33 +0300

    Came back a bottom_navigation_menu.xml

--- app/src/main/java/com/education/notes/presentation/MainActivity.kt
+++ app/src/main/java/com/education/notes/presentation/MainActivity.kt
@@ -3,14 +3,16 @@ package com.education.notes.presentation
 //import androidx.navigation.findNavController
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.education.notes.R
 import com.education.notes.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +25,33 @@ class MainActivity : AppCompatActivity() {
                     it
                 )
             }
+        val bottomNavigationView = binding.mainActivityBottomNavigationView
+        initBottomNavView(bottomNavigationView)
+    }
+
+    private fun initBottomNavView(bottomNavigationView: BottomNavigationView) {
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.bottom_nav_notes_fragment -> {
+                    Navigation.findNavController(this, R.id.fragmentContainerView)
+                        .navigate(R.id.nav_graph_list_fragment)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.bottom_nav_tasks_fragment -> {
+                    Navigation.findNavController(this, R.id.fragmentContainerView)
+                        .navigate(R.id.nav_graph_task_fragment)
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
+        bottomNavigationView.setOnItemReselectedListener { }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.fragmentContainerView)
         return super.onSupportNavigateUp() || navController.navigateUp()
     }
+
+
 }
